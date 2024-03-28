78b4045e100012220d7460139e4b90d7e92f16ad
Oj-washingtone <jalangowashingtone@gmail.com>
2024-03-28 15:55:26 +0300

    fix

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -3,12 +3,7 @@ import React from 'react'
 export default function ProgressBar({ progress, label, description }) {
   return (
     <div className='range'>
-      <div className='progress' style={{ width: `${progress}%`, fontSize: "12px" }}>
-        {label && <span>{label}</span>}
-      </div>
-
-      <div className='progress__label'>{progress}%</div>
-
+      
       {/* custom tooltip */}
       <div className='progress__tooltip'>
         <span style={{fontWeight: "bold", marginBottom: "10px"}}>{label}</span>
@@ -16,6 +11,14 @@ export default function ProgressBar({ progress, label, description }) {
 
         <button className='more__link'>Read More...</button>
       </div>
+
+      <div className='progress' style={{ width: `${progress}%`, fontSize: "12px" }}>
+        {label && <span>{label}</span>}
+      </div>
+
+      <div className='progress__label'>{progress}%</div>
+
+      
     </div>
   )
 }
