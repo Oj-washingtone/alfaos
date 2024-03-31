d335e2117c2a28124cf5ddf81548f953abf9693d
Oj-washingtone <jalangowashingtone@gmail.com>
2024-03-31 15:12:45 +0300

    Crops, and crop disease sections

---
+++ app/dashboard/page.js
@@ -0,0 +1,20 @@
+"use client"
+
+import '@styles/globals.css';
+import Image from "next/image";
+import Menu from "@components/menu";
+import "@components/LineChart"
+import LineChart from '@components/LineChart';
+
+import LeafAnalysis from "@components/LeafAnalysis";
+import ProgressBar from '@components/ProgressBar';
+
+
+export default function Home() {
+  return (
+    <main>
+    TODO: ..........
+ 
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -5,9 +5,11 @@ import Image from "next/image";
 import Menu from "@components/menu";
 import "@components/LineChart"
 import LineChart from '@components/LineChart';
+import { GoAlertFill } from "react-icons/go";
 
 import LeafAnalysis from "@components/LeafAnalysis";
 import ProgressBar from '@components/ProgressBar';
+
 
 
 export default function Home() {
@@ -53,12 +55,9 @@ export default function Home() {
             <div className=" sections col-3">
               <div className="my__card sect_2">
                 <div className='section-header'>
-                  <h2>Crop</h2>
-
-                  <LeafAnalysis />
+                  <h2>Crop Analysis</h2>
                 </div>
-
-                {/* body */}
+                <LeafAnalysis />
               </div>
             </div>
         </div>
@@ -130,8 +129,36 @@ export default function Home() {
           <div className='sections col-4'>
             <div className="my__card sect_4">
                 <div className='section-header'>
-                  <h2>Part 1</h2>
+                  <h2>Crops</h2>
                 </div>
+
+                <div className='row plant-statistics'>
+                  <div className='col-5 crop-card'>
+                    crop 1
+                    <p className='tottal-plants'>1200 plants</p>
+                  </div>
+
+                  <div className='col-5 crop-card'>
+                    crop 2
+                    <p className='tottal-plants'>1200 plants</p>
+                  </div>
+
+                  <div className='col-5 crop-card'>
+                    crop 3
+                    <p className='tottal-plants'>1200 plants</p>
+                  </div>
+
+                  <div className='col-5 crop-card'>
+                    crop 4
+                    <p className='tottal-plants'>1200 plants</p>
+                  </div>
+
+                  <div className='col-5 crop-card'>
+                    crop 5
+                    <p className='tottal-plants'>1200 plants</p>
+                  </div>
+                </div>
+
             </div>
           </div>
           <div className='sections col-4'>
@@ -139,12 +166,47 @@ export default function Home() {
                 <div className='section-header'>
                   <h2>AI Assistant</h2>
                 </div>
+
+                <div className='reccomendations'>
+                  <p>- recommendation 1 Apply fertilizers judiciously based on the needs of the plants and the results of soil tests. Use organic or slow-release fertilizers to provide a steady supply of nutrients over time and minimize the risk of nutrient leaching.</p>
+                  <p>- something else</p>
+                  <p>- another thing </p>
+                  <p>- last thing here</p>
+                </div>
+
+                <div className='card-footer'>
+                  <button className='btn-3'>Read more</button>
+                </div>
             </div>
           </div>
           <div className='sections col-4'>
             <div className="my__card sect_4">
                 <div className='section-header'>
-                  <h2>Part 2</h2>
+                  <h2>Plant Health</h2>
+                </div>
+
+                <div className='reccomendations'>
+                  <div className='warning'>
+                    <GoAlertFill />
+                    <p className='warning-text'> Detected anomaly, channel B2 plant 36 !</p>
+                  </div>
+                  <div className='crop-image-grid row'>
+                    <div className='crop-image-wrapper col-4'>
+                      <Image src="/lettuce.jpeg" width={500} height={500} className='crop-image' alt="Crop disease image"/>
+                    </div>
+                    <div className='crop-image-wrapper col-4'>
+                      <Image src="/lettuce.jpeg" width={500} height={500} className='crop-image' alt="Crop disease image"/>
+                    </div>
+                    <div className='crop-image-wrapper col-4'>
+                      <Image src="/lettuce.jpeg" width={500} height={500} className='crop-image' alt="Crop disease image"/>
+                    </div>
+                  </div>
+
+                  <p className='disease-explanation'>Lettuce Leaf Spot is often caused by fungi such as Cercospora spp., Septoria spp., and Botrytis spp. These fungi can infect lettuce plants, leading to the development of characteristic spots on the leaves. The appearance of the spots can vary depending on the specific pathogen involved, but they often manifest as small, dark lesions or spots on the foliage.</p>
+                </div>
+
+                <div className='card-footer'>
+                  <button className='btn-4'>See full details...</button>
                 </div>
             </div>
           </div>
